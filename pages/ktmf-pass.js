beb1f2ae050ab4334bfbfcf580749d13d0ba9e8f
Dev-Jang <cheonggeum@gmail.com>
2023-07-18 15:22:04 +0900

    contract update

--- pages/ktmf-pass.js
+++ pages/ktmf-pass.js
@@ -43,7 +43,8 @@ const NftSingle = () => {
         const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
         // Estimate the gas limit for the transaction
-        const gasLimit = await contract.estimateGas.publicMint(quantity);
+        //const gasLimit = await contract.estimateGas.publicMint(quantity);
+        const gasLimit = 300000;
 
         // Get the current gas price from the Ethereum network
         const gasPrice = await provider.getGasPrice();
