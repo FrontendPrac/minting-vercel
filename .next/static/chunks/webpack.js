6d981957a0862003a72a332ff2de124fa67e37a4
Dev-Jang <cheonggeum@gmail.com>
2023-08-04 18:07:00 +0900

    Card Slider Fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "805b61247325ff46"; }
+/******/ 		__webpack_require__.h = function() { return "e324208f426a666d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
