b68586141650f3d1b3aa00918bcbf71a6f0d2782
Kidongg <dlrlehd7@daum.net>
2023-08-09 18:00:49 +0900

    Optimizate Video and Image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed4774ddd9e92b85"; }
+/******/ 		__webpack_require__.h = function() { return "ca27d20bdef8ec34"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
