5534574c8261f1ed8d2ab01c9264db679adb6d86
Kidongg <dlrlehd7@daum.net>
2023-08-14 15:15:07 +0900

    Change logo and facicion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "545e8beee9e95b53"; }
+/******/ 		__webpack_require__.h = function() { return "fd4342f20e699571"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
